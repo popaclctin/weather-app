b4b9a52c577a0bc72404f4f7933cfab47fff8204
Claudiu Popa <popa.clctin@gmail.com>
2019-02-24 17:17:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCloud,
+  faCloudRain,
+  faSun,
+  faSnowflake,
+} from '@fortawesome/free-solid-svg-icons';
+const weekDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+const forecastIcon = {
+  sunny: faSun,
+  rainy: faCloudRain,
+  cloudy: faCloud,
+  snowy: faSnowflake,
+};
+
+const daysList = [
+  {
+    date: new Date(2019, 1, 23),
+    high: 1,
+    low: -6,
+    forecast: 'sunny',
+  },
+  {
+    date: new Date(2019, 1, 24),
+    high: 9,
+    low: -2,
+    forecast: 'cloudy',
+  },
+  {
+    date: new Date(2019, 1, 25),
+    high: 9,
+    low: 1,
+    forecast: 'cloudy',
+  },
+  {
+    date: new Date(2019, 1, 26),
+    high: 4,
+    low: -1,
+    forecast: 'snowy',
+  },
+  {
+    date: new Date(2019, 1, 27),
+    high: 12,
+    low: 3,
+    forecast: 'rainy',
+  },
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: daysList,
+    };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { list } = this.state;
+    const result = list.map(day => (
+      <Weather key={day.date.toString()} day={day} />
+    ));
+    return <div className="weather-list">{result}</div>;
   }
 }
+
+const ForecastIcon = props => {
+  return <FontAwesomeIcon icon={forecastIcon[props.value]} />;
+};
+
+const Weather = props => {
+  const { day } = props;
+  return (
+    <div className="weather-card">
+      <div className="weekDay">{weekDay[day.date.getDay()]}</div>
+      <div className="forecastIcon">
+        <ForecastIcon value={day.forecast} />
+      </div>
+      <div className="temperature">
+        <span>{day.high}&deg;</span>
+        <span>{day.low}&deg;</span>
+      </div>
+    </div>
+  );
+};
 
 export default App;
